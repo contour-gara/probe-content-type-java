96c4e95f0dbf46d5c7abc45512db7e59ce60069c
contour-gara <65527071+contour-gara@users.noreply.github.com>
2025-06-09 09:46:02 +0900

    Files を使用した実装を追加

--- src/test/java/org/contourgara/ByFilesTest.java
+++ src/test/java/org/contourgara/ByFilesTest.java
@@ -1,7 +1,26 @@
 package org.contourgara;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ByFilesTest {
+    @ParameterizedTest
+    @CsvSource({
+            "test.png, image/png",
+            "test.jpeg, image/jpeg",
+            "test.txt, text/plain",
+            "てすと.png, image/png"
+    })
+    void ファイル名からContentTypeを取得できる(String fileName, String expected) throws Exception {
+        // setup
+        ByFiles sut = new ByFiles();
 
+        // execute
+        String actual = sut.execute(fileName);
+
+        // assert
+        assertThat(actual).isEqualTo(expected);
+    }
 }
